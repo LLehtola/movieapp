3c99c39562a144eca92ee8e8aadebf128fe05897
lauri.lehtola <mr.lauri.lehtola@gmail.com>
2021-09-28 23:48:26 +0300

    feat: completed MovieService

--- src/main/java/fi/experis/movieapp/services/MovieService.java
+++ src/main/java/fi/experis/movieapp/services/MovieService.java
@@ -1,6 +1,22 @@
 package fi.experis.movieapp.services;
 
+import fi.experis.movieapp.models.Movie;
+import fi.experis.movieapp.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
 public class MovieService {
 
+    @Autowired
+    private MovieRepository movieRepository;
 
+    public Movie saveMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
 }
