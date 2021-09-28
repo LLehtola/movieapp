7354a0f9b5db4d9ca02135b44a4fbacd0bbb3d7d
lauri.lehtola <mr.lauri.lehtola@gmail.com>
2021-09-28 23:48:06 +0300

    feat: completed MovieController

--- src/main/java/fi/experis/movieapp/controllers/MovieController.java
+++ src/main/java/fi/experis/movieapp/controllers/MovieController.java
@@ -1,4 +1,41 @@
 package fi.experis.movieapp.controllers;
 
+import fi.experis.movieapp.models.Movie;
+import fi.experis.movieapp.repositories.MovieRepository;
+import fi.experis.movieapp.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/movies")
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie newMovie = movieService.saveMovie(movie);
+        HttpStatus status = HttpStatus.CREATED;
+        return new ResponseEntity<>(newMovie, status);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        List<Movie> movies = movieService.getAllMovies();
+        HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(movies, status);
+    }
+
+
+
 }
